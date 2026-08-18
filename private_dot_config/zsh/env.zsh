925eeb8dd3ed2da6a1f1eb83ef494c4503778270
Ahmed Halim <ahmedabdelhalim1996@gmail.com>
2026-08-18 07:29:51 +0300

    Update .config/zsh/env.zsh

--- private_dot_config/zsh/env.zsh
+++ private_dot_config/zsh/env.zsh
@@ -3,7 +3,7 @@
 # android
 export ANDROID_HOME="$HOME/Android/Sdk"
 export ANDROID_SDK_ROOT="$ANDROID_HOME"
-export NDK_HOME="$ANDROID_HOME/ndk/29.0.13846066"
+export NDK_HOME="$ANDROID_HOME/ndk/30.0.15729638"
 # SDK Tool Paths
 export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
 export PATH="$PATH:$ANDROID_HOME/platform-tools"
